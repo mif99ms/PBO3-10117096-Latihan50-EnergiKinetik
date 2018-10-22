d34714d06bae1e39b5c4f30fc356115838969c88
mif99ms <msidqi86@gmail.com>
2018-10-22 23:01:25 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan50/energikinetik/PBO310117096Latihan50EnergiKinetik.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan50.energikinetik;
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO3
+ * Deskripsi    : Program ini berisi program untuk menghitung energi kinetik
+ */
+public class PBO310117096Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Energi kinetik = new Energi();
+        kinetik.setMassa(0.145);
+        kinetik.setKecepatan(25);
+        kinetik.setKecepatanAwal(0);
+        kinetik.setKecepatanAkhir(25);
+        
+        DecimalFormat df = new DecimalFormat("#.#");
+        
+        System.out.println("=====PROGRAM MENCARI ENERGI KINETIK=====");
+        System.out.println("Energi Kinetik : " + df.format(
+                           kinetik.hitungEnergiKinetik
+                          (kinetik.getMassa(), kinetik.getKecepatan())));
+        System.out.println("Usaha : " + df.format (kinetik.hitungUsaha
+                          (kinetik.getKecepatanAwal(), 
+                           kinetik.getKecepatanAkhir())));
+    }
+    
+}
